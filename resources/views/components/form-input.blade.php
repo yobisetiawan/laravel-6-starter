1db155c1cf2e20c88229638c639780e2adb9211c
Yobi Bina Setiawan <yobi.studio@gmail.com>
2021-11-02 09:40:48 +0700

    update comp

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,8 +1,10 @@
 <div class="form-group">
+    @if(!empty($label))
     <label>{{$label}}</label>
+    @endif
 
     <div>
-        <input type="{{$type ?? 'text'}}" placeholder="{{$placeholder ?? $label}}" class="form-control @error($field) is-invalid @enderror" name="{{$field}}" value="{{ old($field) ?? ($row->$field ?? '') }}" />
+        <input type="{{$type ?? 'text'}}" placeholder="{{$placeholder ?? $label}}" class="form-control @error($field) is-invalid @enderror" name="{{$field}}" value="{{ old($field, ($row->$field ?? '')) }}" />
         @error($field)
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
